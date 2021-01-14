349f6043e3c514c5b5e8c781579c67dceeecb74e
Giovanna Manfredo <giovanna.cmanfredo@gmail.com>
2021-01-14 18:51:21 -0300

    first commit

---
+++ Fundamentos/organizacão.js
@@ -0,0 +1,32 @@
+
+// o codigo é dividido em sentenças
+console.log("Sentença de código")
+
+{
+    {
+        console.log("Sentença no bloco")
+    }
+    //blocos agrulpam sentenças de códigos. 
+
+}  
+
+// comentarios de codigos.
+
+// Comentarios de uma única linha
+
+/*
+Comentário de multiplas linhas
+*/
+
+/*
+ * Comentarios mais "estiloso"
+ */
+
+ /*
+    Principios de comentarios.
+
+    comentarios obveis.
+    Faça um codigo limpo.
+    codigo com bons nomes.
+
+ */
---
+++ Fundamentos/variaveis_e_constantes.js
@@ -0,0 +1,19 @@
+var nomeVariavel = 3 
+let outraVariavel = 4 //forma mais moderna de criar variavel
+
+var nomeVariavel = 30 
+// let outraVariavel = 40 -> Aqui da erro, so o var deixa redeclarar a mesma variavel 2 vezes.
+
+console.log(nomeVariavel)
+
+const c = 5 //constante nao podem ser alteradas
+
+// Tipagem fraca -> as variaveis não precisam declarar seu tipo, logo ela é mais flexivel.
+
+let textual = "Sou do tipo textual"
+let valorReal = 3.1416
+
+console.log(typeof textual)
+
+
+
